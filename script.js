826d018f18518e518bf0e214d65f4dbdef5a5bd5
Divesh Rajput <rajpootdivesh5@gmail.com>
2024-08-14 23:29:41 +0530

    added websites pages

---
+++ script.js
@@ -0,0 +1,74 @@
+let menu = document.querySelector("#menu-btn");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+};
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("header").style.top = "0";
+  } else {
+    document.getElementById("header").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+};
+
+
+
+var form = document.getElementById("my-form");
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("my-form-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        alert("Thanks for your submission!");
+        form.reset();
+      } else {
+        response.json().then((data) => {
+          if (Object.hasOwn(data, "errors")) {
+            status.innerHTML = data["errors"]
+              .map((error) => error["message"])
+              .join(", ");
+          } else {
+            alert("Oops! There was a problem submitting your form");
+          }
+        });
+      }
+    })
+    .catch((error) => {
+      alert("Oops! There was a problem submitting your form");
+    });
+}
+form.addEventListener("submit", handleSubmit);
+
+
+input.addEventListener("keypress", function(event){
+  if(event.key==="Enter"){
+    event.preventDefault();
+    document.getElementById("my-form-button").click();
+  }
+});
+
+
+
+    
+
